1a51bdebe178bea49c6bfc5c630ec245f375e405
ly107253 <wangjun8882008@126.com>
2020-02-12 17:47:43 +0800

    at24c02/删除非源码文件
    删除非源码文件

--- at24c02/at24c02/.at24c02_drv.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/opt/drivers/at24c02/at24c02/at24c02_drv.ko := arm-linux-ld -EL -r  -T /opt/EmbedSky/TQ210/Kernel_3.0.8_TQ210_for_Linux_v2.1/scripts/module-common.lds --build-id  -o /opt/drivers/at24c02/at24c02/at24c02_drv.ko /opt/drivers/at24c02/at24c02/at24c02_drv.o /opt/drivers/at24c02/at24c02/at24c02_drv.mod.o
--- at24c02/at24c02/.tmp_versions/at24c02_drv.mod
+++
@@ -1,2 +0,0 @@
-/opt/drivers/at24c02/at24c02/at24c02_drv.ko
-/opt/drivers/at24c02/at24c02/at24c02_drv.o
