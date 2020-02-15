fe15896ad7330c20ba0b246301f0f17841272572
ly107253 <wangjun8882008@126.com>
2020-02-15 12:49:44 +0800

    update rtc.c
    调整时间读取接口

--- rtc/rtc.c
+++ rtc/rtc.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 	{
 		printf("打开时钟失败\n");		
 	}
+	#if 0
 	rtc_tm.tm_year = 2020;
 	rtc_tm.tm_mon = 02;
 	rtc_tm.tm_mon = 14;
@@ -26,8 +27,9 @@ int main(int argc, char *argv[])
 	rtc_tm.tm_min = 50;
 	
 	ioctl(fd, RTC_SET_TIME, &rtc_tm);
+	#endif
 	ioctl(fd, RTC_RD_TIME, &rtc_tm);
-	printf("%d %d %d %d %d\n",rtc_tm.tm_year,rtc_tm.tm_mon,rtc_tm.tm_mday,rtc_tm.tm_hour,rtc_tm.tm_min);
+	printf("%d %d %d %d %d\n",rtc_tm.tm_year+1900,rtc_tm.tm_mon+1,rtc_tm.tm_mday,rtc_tm.tm_hour,rtc_tm.tm_min);
     return 0;
 }
 
